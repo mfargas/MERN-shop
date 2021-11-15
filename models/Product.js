3b7b8af349f0945fdabfff483421954eaf9d39bb
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-11-15 12:19:06 -0800

    Import products json array to mongodb

--- models/Product.js
+++ models/Product.js
@@ -1,3 +1,31 @@
 import mongoose from 'mongoose'
+import shortid from 'shortid'
 
-new.mongoose.Schema({})
+const { String, Number } = mongoose.Schema.Types;
+
+const ProductSchema = new.mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    sku:{
+        type: String,
+        required: true,
+        unique: true,
+        default: shortid.generate()
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    mediaUrl:{
+        type: String,
+        required: true
+    }
+})
+
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema)
